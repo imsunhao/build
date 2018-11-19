5bc2c3a3c0ef689f381f372c6dc4dbb316e7a2bd
孙颢 <sunhao@bestminr.com>
2018-11-19 14:14:58 +0800

    build 的 同时 删除 output path文件夹

--- src/build.ts
+++ src/build.ts
@@ -1,8 +1,14 @@
 import { getConfig } from 'src/utils'
+import rimraf from 'rimraf'
 import { compiler } from 'src/utils/compiler.webpack.ts'
 
 export function serverBuild() {
   const config = getConfig()
+
+  if (config.webpack && config.webpack.base && config.webpack.base.output) {
+    rimraf.sync(config.webpack.base.output.path || '')
+  }
+
   const clientConfig: any = config.webpack ? config.webpack.client || {} : {}
   const serverConfig: any = config.webpack ? config.webpack.server || {} : {}
 
